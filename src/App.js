177e578d6c71793bc60c8d04aa6939efa9559612
kensteph <=kenderromain@gmail.com>
2022-09-17 18:08:34 -0400

    Adding home,hotels list,hotel pages

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  BrowserRouter,
+  Routes
+} from "react-router-dom";
+import { Home } from "./home/Home";
+import { Hotel } from "./hotel/Hotel";
+import { List } from "./list/List";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+       </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
